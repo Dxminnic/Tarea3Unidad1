a8a7b0a46941acb1cf431ef321974942f3060843
Dxminnic <dominicbarron31@gmail.com>
2026-09-23 23:59:52 -0600

    Subir backend con main.py y configuracion

--- backend/config/database.py
+++ backend/config/database.py
@@ -1,13 +1,35 @@
 import os
-from psycopg.row import dict_row
+
+import psycopg
+from psycopg.rows import dict_row
 from dotenv import load_dotenv
 
-#carga las variables del"
 
+# Cargar las variables del archivo .env
 load_dotenv()
 
-DB_HOST = os.get_env("DB_HOST")
-DB_PORT = os.get_env("DB_HOST")
-DB_ = os.get_env("DB_HOST")
-DB_HOST = os.get_env("DB_HOST")
-DB_HOST = os.get_env("DB_HOST")
+
+# Variables de conexión
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+
+def get_connection():
+    """
+    Crea y devuelve una conexión a PostgreSQL.
+    """
+
+    connection = psycopg.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        row_factory=dict_row,
+        connect_timeout=5
+    )
+
+    return connection
