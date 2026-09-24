f7feb7b952adc15e6564ebf8fe27361ba27abbac
Dxminnic <dominicbarron31@gmail.com>
2026-09-23 23:53:08 -0600

    Tarea 3 Unidad 1

---
+++ backend/config/database.py
@@ -0,0 +1,13 @@
+import os
+from psycopg.row import dict_row
+from dotenv import load_dotenv
+
+#carga las variables del"
+
+load_dotenv()
+
+DB_HOST = os.get_env("DB_HOST")
+DB_PORT = os.get_env("DB_HOST")
+DB_ = os.get_env("DB_HOST")
+DB_HOST = os.get_env("DB_HOST")
+DB_HOST = os.get_env("DB_HOST")
